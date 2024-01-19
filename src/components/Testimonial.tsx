00fdbeb2634f3ed004861bfe0d73b7f83a3cae52
ridhoisme <ridho@koda.so>
2024-01-19 23:32:04 +0700

    add image testimony

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -2,6 +2,38 @@ import Autoplay from "embla-carousel-autoplay";
 import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
 import StarIcon from "@/assets/icons/Star";
 import Person4 from "@/assets/images/person-4.png";
+import Person5 from "@/assets/images/person-5.png";
+import Person6 from "@/assets/images/person-6.png";
+import Person7 from "@/assets/images/person-7.png";
+import Person8 from "@/assets/images/person-8.png";
+
+const listTestimony = [
+  {
+    id: 1,
+    img: Person4,
+    name: "Hendra Paru Paru",
+  },
+  {
+    id: 2,
+    img: Person5,
+    name: "Nichole Edward",
+  },
+  {
+    id: 3,
+    img: Person6,
+    name: "Jonathan R.",
+  },
+  {
+    id: 4,
+    img: Person7,
+    name: "Akbar Cenglu",
+  },
+  {
+    id: 5,
+    img: Person8,
+    name: "Akbar Cenglu",
+  },
+];
 
 const VerticalCarousel = ({
   delay = 2000,
@@ -21,8 +53,8 @@ const VerticalCarousel = ({
       className="w-full"
     >
       <CarouselContent className="-mt-1 h-[900px] w-full">
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index} className="md:basis-1/3 mb-4 mx-4">
+        {listTestimony.map((testimoni) => (
+          <CarouselItem key={testimoni.id} className="md:basis-1/3 mb-4 mx-4">
             <div className="flex flex-col gap-6 shadow-testimonial h-full w-full p-6 rounded-lg">
               <div className="flex gap-1">
                 {Array.from({ length: 5 }).map((_, index) => (
@@ -38,14 +70,14 @@ const VerticalCarousel = ({
               <div className="flex gap-4">
                 <div className="w-12">
                   <img
-                    src={Person4}
+                    src={testimoni.img}
                     alt=""
                     className="rounded-full aspect-square"
                   />
                 </div>
                 <div className="flex flex-col items-start">
-                  <span className="font-bold text-lg">Jake Anderson</span>
-                  <p className="text-brand-gray">Best Tour Guide</p>
+                  <span className="font-bold text-lg">{testimoni.name}</span>
+                  <p className="text-brand-gray">Travel Enthusiast</p>
                 </div>
               </div>
             </div>
